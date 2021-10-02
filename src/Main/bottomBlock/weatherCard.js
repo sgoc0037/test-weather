12f1475ba34ca8076434f7222e1212015c72a03b
sgoc0037 <wazqwe.joker@mail.ru>
2021-10-02 13:48:58 +0300

    ...

--- src/Main/bottomBlock/weatherCard.js
+++ src/Main/bottomBlock/weatherCard.js
@@ -3,13 +3,22 @@ import style from './weatherCard.module.css';
 
 const WeatherCard = (props) => {
 
+    const getTemp = (value)=> {
+        
+        if(value.length > 2) {
+            return value
+        } else {
+            return `+${value}`
+        }
+    }
+
     return <div className={style.weatherCard}>
         <b className={style.changePosition}>{props.date[0]}</b>
         <span className={style.lightGray+ ' ' + style.midValue}>{props.date[1]}</span>
         <img src={props.icon} alt='icon' />
         <div>
-            <b>{props.minTemp}&deg;</b>
-            <span className={style.lightGray}>{props.maxTemp}&deg;</span>
+            <b>{getTemp(props.minTemp)}&deg;</b>
+            <span className={style.lightGray}>{getTemp(props.maxTemp)}&deg;</span>
         </div>
         <span className={style.lightGray+ ' ' + style.midValue}>{props.condition}</span>
     </div>
